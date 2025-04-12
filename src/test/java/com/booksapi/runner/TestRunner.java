3b7a3f5645500f64ce2f5925f16c55237d008add
Alena.Badzilouskaya <badzilouskayaale@sbsit.com>
2025-04-12 11:22:28 +0200

    Test runner updated

--- src/test/java/com/booksapi/runner/TestRunner.java
+++ src/test/java/com/booksapi/runner/TestRunner.java
@@ -1,14 +1,16 @@
 package com.booksapi.runner;
 
-import io.cucumber.core.options.Constants;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
+
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "steps")
-@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty, html:build/reports/cucumber-report.html")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:build/reports/cucumber-report.html")
 public class TestRunner {}
